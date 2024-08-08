9bf293c4c55d679b2c75e3a8651663f44f1e9dc9
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-08 11:48:33 +0530

    Create readthegivenarray.cpp

---
+++ readthegivenarray.cpp
@@ -0,0 +1,18 @@
+//kshitij yadav 23070123075
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x[5] = {11, 27, 32, 46, 50};
+
+
+    cout << "Array elements: ";
+    for (int i = 0; i < 5; i++) {
+       cout << x[i] << "/n";
+    }
+
+    return 0;
+}
+/*
+Array elements: 11 27 32 46 50 
+*/
