b5abc53832517b5ceb3bf2814b12af6da7ab5aa8
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-09-02 12:26:53 +0530

    Create concatenationofstring.cpp

---
+++ concatenationofstring.cpp
@@ -0,0 +1,11 @@
+//kshitij 23070123075
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Soham";
+    last_name = "Jade";
+
+    cout<<first_name+last_name;
+}
