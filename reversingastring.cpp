880cd070a36c29baf934c281236909a902753dd6
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-09-02 12:28:07 +0530

    Create reversingastring.cpp

---
+++ reversingastring.cpp
@@ -0,0 +1,15 @@
+//kshitij 23070123075
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cout<<"Enter a string: ";
+    cin>>a;
+    int n = a.length();
+    
+    for(int i=n-1;i>=0;i--)
+    {
+        cout<<a[i];
+    }
+}
